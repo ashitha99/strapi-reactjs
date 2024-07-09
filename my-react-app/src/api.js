2979e9881f49b16d1f90479e4e1ecd02b8faf5e5
ashitha99 <ashithavedody@gmail.com>
2024-07-09 07:53:33 +0530

    First Commit

---
+++ my-react-app/src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+export const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:1337/api/articles');
+    // Assuming Strapi returns data in response.data.data
+    return response.data.data;
+  } catch (error) {
+    throw new Error('Error fetching data from Strapi');
+  }
+};
+
